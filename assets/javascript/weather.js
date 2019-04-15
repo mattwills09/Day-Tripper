c83ca194ece19e743911801df139273b2bd2f27d
mattwills09 <mattwills09@gmail.com>
2019-04-15 11:22:26 -0400

    Created functions for Zip & Lat/Lon searches

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,12 +1,4 @@
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=99501&units=imperial&appid=6832cd13112f3ff58acaee5e7646c57a";
-
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-
-    console.log(response);
-
+function processData(response){
     $(".city").text(response.city.name + "'s Current Weather");
     $(".temp3").text("Current Temperature (F): " + response.list[0].main.temp);
     $(".conditions").text("Current Conditions: " + response.list[0].weather[0].description);
@@ -14,28 +6,32 @@ $.ajax({
     $(".temp6").text("Forecasted Temperature (Next 6 hrs): " + response.list[1].main.temp);
     $(".conditions").text("Forecasted Conditions: " + response.list[1].weather[0].description);
 
+
     //-----------------------------------------------------------------
 
     var suggestionDay = response.list[0].main.temp;
 
     if (suggestionDay >= 60)
-        $(".suggestDay").text("Woot!  Looks like SHORTS today!");
+        $(".suggestDay").text("Woot!  Jen's kids can wear SHORTS today!");
     else
-        $(".suggestDay").text("Nope, today looks like PANTS weather.. bummer");
+        $(".suggestDay").text("No shorts today, looks like you should bring a jacket also.. bummer");
 
     var suggestionNight = response.list[0].main.temp;
 
     if (suggestionNight >= 60)
         $(".suggestNight").text("Keep those shorts for later!");
     else
-        $(".suggestNight").text("Might need pants later, looks a little chilly.");
+        $(".suggestNight").text("Most likely you'll need a jacket or sweater later, looks a little chilly.");
 
     //-----------------------------------------------------------------
-
+    // 3-hour / Current Weather
     var cloudCover = response.list[0].clouds.all;
     var rainChance = response.list[0].weather[0].main;
+    // 6-hour / Forecasted Weather
+    var cloudCover6 = response.list[1].clouds.all;
+    var rainChance6 = response.list[1].weather[0].main;
 
-    console.log(rainChance);
+        console.log(rainChance);
 
     if (rainChance == "Rain") {
         $("#welcomeWeather").attr("src", "assets/images/rainy.png");  
@@ -50,5 +46,53 @@ $.ajax({
         $("#welcomeWeather").attr("src", "assets/images/sunny.jpg");
     }
 
+    if (rainChance6 == "Rain") {
+        $("#forecastWeather").attr("src", "assets/images/rainy.png");  
+    }
+    else if(cloudCover6 >= 75) {
+        $("#forecastWeather").attr("src", "assets/images/cloudy.png");
+     }
+    else if(cloudCover6 >= 50) {
+        $("#forecastWeather").attr("src", "assets/images/partly-cloudy.png");
+     }
+    else {
+        $("#forecastWeather").attr("src", "assets/images/sunny.jpg");
+    }
+};
 
-});
+//-----------------ZIP CODE API QUERY------------------------
+
+zipSearch(99501);
+
+function zipSearch(zipCode){
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=" + zipCode + "&units=imperial&appid=6832cd13112f3ff58acaee5e7646c57a";
+
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function(response) {
+
+        processData(response);
+        console.log(response);
+
+    });
+}
+
+//-----------------LATITUDE/LONGITUDE API QUERY------------------------
+
+// lat/lon of pennovation: 39.941433, -75.199385
+// geoSearch(29.76, -95.36);
+
+function geoSearch(latitude, longitude){
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast/hourly?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=6832cd13112f3ff58acaee5e7646c57a";
+
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function(response) {
+
+        processData(response);
+        console.log(response);
+
+    });
+}
