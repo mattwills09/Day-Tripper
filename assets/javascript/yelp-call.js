4af26d07ad64f49d98efc11ea492d829391c38be
bigalr8 <bigalr8@icloud.com>
2019-04-15 13:32:20 -0400

    functional coord search prototype

--- assets/javascript/yelp-call.js
+++ assets/javascript/yelp-call.js
@@ -21,6 +21,7 @@ $(document).ready(function () {
   
     //POI object constructor takes a JSON object returned by yelp API call
     function POI(response) {
+      console.log("POI");
       this.name = response.name, //business name
         this.street = response.location.display_address[0], //building number, street name
         this.city = response.location.display_address[1], //city, zip code
@@ -29,11 +30,12 @@ $(document).ready(function () {
         this.phone = response.display_phone //phone number
     }
   
-    getPOIsCOORDS("Martial", "39.9553076", "-75.1720374")
+    //getPOIsCOORDS("Martial", "39.9553076", "-75.1720374")
   
   
     //Function to make a yelp API. Takes category(string) and location (zip code number)
     function getPOIsZIP(category, location) {
+      console.log("getPOIZIP");
       var category = category;
       var location = location;
   
@@ -67,6 +69,7 @@ $(document).ready(function () {
   
     //function to make a yelp API call, takes 3 parameters: category (string), latitude (int), and longitude (int)
     function getPOIsCOORDS(category, lat, long) {
+      console.log("getPOICOORDS - category: " + category);
       var category = category;
       var lat = lat;
       var long = long;
@@ -95,7 +98,8 @@ $(document).ready(function () {
         //   var yelpID = locations[i].id; //get the business id
         //   getPOIdetails(yelpID); //send the id to getPOIdetails for a second api call
         // }
-  
+        console.log("$.ajax(settings) - locations: " + JSON.stringify(locations[0].id));
+        console.log("$.ajax(settings) - response.business: " + JSON.stringify(response.businesses));
         getPOIdetails(locations[0].id);
         getPOIdetails(locations[1].id);
         getPOIdetails(locations[2].id);
@@ -106,7 +110,7 @@ $(document).ready(function () {
   
     //function to make a yelp API call, takes 1 parameter: yelpID (string)
     function getPOIdetails(yelpID) {
-  
+      console.log("getPOIdetails");
       //yelp query url to search by business ID
       var queryURL = "https://cors-anywhere.herokuapp.com/http://api.yelp.com/v3/businesses/" + yelpID;
   
@@ -125,13 +129,13 @@ $(document).ready(function () {
   
       $.ajax(settings).then(function (response) {
         var poi = new POI(response);
-      
-        // console.log(poi.street);
-        // console.log(poi.city);
-        // console.log(poi.phone);
-        // console.log(poi.link);
-        // console.log(poi.hours);
-        // console.log(poi);
+        console.log("new POI response - yelpResults" + yelpResults);
+        console.log(poi.street);
+        console.log(poi.city);
+        console.log(poi.phone);
+        console.log(poi.link);
+        console.log(poi.hours);
+        console.log(poi);
         yelpResults.push(poi);
         publishResults(yelpResults);
       })
@@ -140,7 +144,7 @@ $(document).ready(function () {
   
     //dynamically makes buttons based on categories users are allowed to search for
     function makeButton(name){
-      var name = name;
+      console.log("makeButton");
       var btn = $("<button>");
       btn.text(name);
       btn.attr({"id":name,"data-selected":"false"});//set default to data-selected = false
@@ -177,17 +181,19 @@ $(document).ready(function () {
       $("#results").append(btn); //add to results div 
     }
   
-    var test = ["garden","zorbing","museum"];
+    /* var test = ["garden","zorbing","museum"];
     test.forEach(function(item){
       makeButton(item);
     });
-  
+  */
   
   
     function publishResults(yelpResults){
-      console.log("here");
+      console.log("publishResults");
+     //console.log("here");
       //for each of the yelp results
       yelpResults.forEach(function(result){
+        
         var name = result.name;
         var street = result.street;
         var city = result.city;
@@ -198,28 +204,32 @@ $(document).ready(function () {
         div.attr({"id":result.name});
   
         div.append(addName(name)).append(addAddress(street,city)).append(addPhone(phone)).append(addLink(link));
-        console.log(div);
+        //console.log(div);
         $("#results").append(div);
         function addName(name){
+          //console.log("addName");
           var nameTag = $("<h3>");
           nameTag.text(name);
-          console.log(nameTag);
+          //console.log(nameTag);
           return nameTag;
      
         }
         function addAddress(street,city){
+          //console.log("addAddress");
           var addressTag = $("<p>");
           addressTag.text(street + ", "+city);
           
           return addressTag;
         }
         function addPhone(phone){
+          //console.log("addPhone");
           var phoneTag = $("<p>");
           phoneTag.text(phone);
   
           return phoneTag;
         }
         function addLink(link){
+          //console.log("addLink");
           var linkTag = $("<a>");
           linkTag.attr({"href":link}).text("Open in Yelp");
   
@@ -228,6 +238,184 @@ $(document).ready(function () {
       });
     }
   
+//==================================================================================================================================================    
+
+//  B U T T O N   O B J E C T 
+//  <button class="activitySettingButton btn btn-outline-primary" data-activitySetting="InOrOut">InOrOut</button>... 
+var btnObj = {
+  clkCls: "activitySettingsButton",
+  stylCl: "btn btn-outline-primary",
+  dataLbl: "data-activitySetting",
+  arr: ["Outdoors", "Indoors", "InOrOut"],
+  anchor: "#main-buttons-anchor",
+//  r e n d o r B u t t o n   F U N C T I O N 
+  renderButtons: function () {
+      //console.log("render buttons: " + this.arr);
+      for (i = 0; i < this.arr.length; i++) {
+          var newBtn = $("<button>");
+
+          //console.log("clkCls: " + this.clkCls);
+          newBtn.addClass(this.clkCls);
+
+          //console.log("stylCls: " + this.stylCls);
+          newBtn.addClass(this.stylCls);
+
+          //console.log("dataLbl: " + this.dataLbl);
+          newBtn.attr(this.dataLbl, this.arr[i]);
+
+          
+          newBtn.text(this.arr[i]);
+
+          //console.log("anchor: " + this.anchor);
+          $(this.anchor).append(newBtn);
+      }
+  },
+};
+
+
+// " a c t i v i t y S e a r c h ( )"   F U N C T I O N -  S E A R C H   B R O A D   A N D   A C T I V I T Y   C A T E G O R Y   O B J E C T   A R R A Y S      
+// var broadCategory = [{activity: "Wild Life Gaming", setting: "Outdoors" },...
+// var activityCategory = [{activity:"bungeejumping", category:"Adventure sports"},...
+function activitySearch(myArray, prop, nameKey) {
+//  console.log("myArray: " + myArray);
+  var newArr = [];
+  for (var i = 0; i < myArray.length; i++) {
+//      console.log(myArray[i][prop]);
+      if (myArray[i][prop] === nameKey) {
+          newArr.push(myArray[i].activity);
+      }
+  }
+  return newArr;
+}
+
+//  R E N D E R   B U T T O N S   F O R   A C T I V I T Y   S E T T I N G S - i.e indoors, outdoors, in-or-out  
+//  Pass arguments to button object  
+//  var activitySettingArr = ["InOrOut", "Outdoors", "Indoors"];... 
+  btnObj.arr = activitySettingArr;
+//  console.log("arr: " + btnObj.arr);
+  btnObj.anchor = ("#main-buttons-anchor");
+//  console.log("anchor: " + btnObj.anchor);
+
+
+//  C A L L   r e n d e r B u t t o n s ( )   -  B U I L D   S E T T I N G   B U T T O N S
+//  <button class="activitySettingsButton btn btn-outline-primary" data-activitySetting="InOrOut">InOrOut</button>... 
+  btnObj.renderButtons();
+
+
+//  S E T T I N G   B U T T O N   C L I C K   E V E N T  - find broad categories for that setting
+  $(".activitySettingsButton").on("click", function (event) {
+//      Prevent form submission
+      event.preventDefault();
+
+//      Store button selection for activity setting - indoors, outdoors, both
+//      <button class="activitySettingsButton btn btn-outline-primary" data-activitySetting="InOrOut">InOrOut</button> 
+      var selectedButton = $(this).attr("data-activitySetting");
+//      console.log("selectedButton: " + selectedButton);
+
+
+//      C A L L   a c t i v i t y S e a r c h ( ) F U N C T I O N   
+//      T O   F I N D   B R O A D   C A T E G O R I E S   O F   A C T I V I T I E S   A P P R O P R I A T E   T O   S E L E C T E D   A C T I V I T Y   S E T T I N G      
+//      A N D   C R E A T E   A R R A Y   O F   R E S U L T S   F O R   B U T T O N S  
+//      var broadCategory = [{ activity: "Wild Life Gaming", setting: "Outdoors" },
+      var broadArr = activitySearch(broadCategory, "setting", selectedButton);
+//      console.log("broadArr" + broadArr);
+
+//====================================================================================================================================================
+//      R E N D E R   B U T T O N S   F O R   B R O A D   C A T E G O R I E S   S U I T E D   T O   S E L E C T E D   A C T I V I T Y   S E T T I N G   
+//      <button class="categoryButton" data-broad="Adventure sports">Adventure sports</button>...
+//      Pass arguments to button object 
+      btnObj.clkCls = "categoryButton",
+      btnObj.arr = broadArr;
+//      console.log("arr: " + btnObj.arr);
+      btnObj.anchor = ("#broad-buttons-anchor");
+      $(btnObj.anchor).empty();
+//      console.log("anchor: " + btnObj.anchor);
+      btnObj.dataLbl = "data-broad",
+
+
+//      C A L L   r e n d e r B u t t o n s ( ) F U N C T I O N 
+      btnObj.renderButtons();
+
+
+//      E N D   S E T T I N G   B U T T O N   C L I C K   E V E N T   H A N D L I N G   
+});
+
+
+// B R O A D   C A T E G O R Y   B U T T O N   C L I C K   E V E N T - find yelp categories for broad category          
+// Using event delegation on parent object to ensure event listener operates on secondary button event
+$(document).on('click', '.categoryButton', function () {
+  let button = $(this).parent('#broad-buttons-anchor');
+
+
+//  Prevent form submission
+  event.preventDefault();
+
+
+//  Store broad activity category selection   
+//  <button class="categoryButton" data-broad="Adventure sports">Adventure sports</button>
+  var selectedButton = $(this).attr("data-broad");
+//  console.log("selectedButton: " + selectedButton);
+
+
+//  C A L L   a c t i v i t y S e a r c h ( ) F U N C T I O N   
+//  T O   F I N D   Y E L P   S E A R C H    C A T E G O R I E S   F O R   A C T I V I T I E S   I N   S E L E C T E D   B R O A D   C A T E G O R Y      
+//  A N D   C R E A T E   A R R A Y   O F   R E S U L T S   F O R   B U T T O N S 
+//  {activity:"bungeejumping", category:"Adventure sports"},
+//  console.log("selectect Button: " + selectedButton);
+  var searchArr = activitySearch(activityCategory, "category", selectedButton);
+
+//====================================================================================================================================================
+//      R E N D E R   B U T T O N S   F O R   Y E L P  C A T E G O R I E S   S U I T E D   T O   S E L E C T E D   B R O A D   C A T E G O R Y   
+//      <button class="searchButton" data-search="Bungie Jumping">Bungie Jumping</button>...
+//      Pass arguments to button object 
+btnObj.clkCls = "searchButton",
+btnObj.arr = searchArr;
+//console.log("arr: " + btnObj.arr);
+btnObj.anchor = ("#search-buttons-anchor");
+$(btnObj.anchor).empty();
+//console.log("anchor: " + btnObj.anchor);
+btnObj.dataLbl = "data-search",
+//console.log("btnObj:  " + btnObj);
+
+//      C A L L   r e n d e r B u t t o n s ( ) F U N C T I O N 
+btnObj.renderButtons();
+});
+
+// S E A R C H  C A T E G O R Y   B U T T O N   C L I C K   E V E N T - find yelp categories for broad category          
+// Using event delegation on parent object to ensure event listener operates on secondary button event
+$(document).on('click', '.searchButton', function () {
+  let button = $(this).parent('#search-buttons-anchor');
+
+
+//  Prevent form submission
+  event.preventDefault();
+
+
+//  Store broad activity category selection   
+//  <button class="categoryButton" data-broad="Adventure sports">Adventure sports</button>
+  var selectedButton = $(this).attr("data-search");
+  console.log("selectedButton: " + selectedButton);
+getPOIsCOORDS(selectedButton, "39.9553076", "-75.1720374")
+
+//  Render search results on page    
+  //$("#categories-anchor").html("<p></p>");
+  //$("p").text(searchArr);
+  //console.log("click category - searchArr: " + searchArr);
+
+  //for(i=0; i< searchArr.length; i++) {
+    
+  //}
+  //==========================================================================================================================================
+
+
+  //searchArr.forEach(getPOIsCOORDS(searchArr[i], "39.9553076", "-75.1720374"));
+  //searchArr.forEach(myFunction);
+ 
+  //function myFunction(item, index) {
+  //  console.log( "index[" + index + "]: " + item ); 
+  //  getPOIsCOORDS(item, "39.9553076", "-75.1720374")
+  //}
+});
     /*** END YELP***/
   
     //close document ready  
